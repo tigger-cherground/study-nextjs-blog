209b8ef56ce1423723fa96b8ec5a0603b518fb95
tigger <tigger@cherground.com>
2023-11-16 11:46:01 +0900

    Layouts - Styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,11 @@
+import Head from 'next/head';
+import Layout from '../../components/Layout'
+
 export default function FirstPost() {
-    return <h1>첫번째 글</h1>
+    return <Layout>
+        <Head>
+            <title>첫번째 글</title>
+        </Head>
+        <h1>첫번째 글</h1>
+    </Layout>
 }
