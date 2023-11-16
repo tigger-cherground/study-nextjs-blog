77943641cd73b1fcd1fac460e30db3028b557034
tigger <tigger@cherground.com>
2023-11-16 14:43:19 +0900

    Pre-rendering - Data fetching

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/global.css'
 
-export default function App({ Component, pagePros }) {
-    return <Component {...pagePros} />
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
 }
